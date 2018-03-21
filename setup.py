9351b648f4af55b7ab10b492ba69d2b9a893145f
Dinesh Yadav <27940059+dineshvipe@users.noreply.github.com>
2018-03-21 20:56:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Dinesh Yadav',
   author_email = 'dineshyad72@gmail.com',
   url = 'https://github.com/dineshvipe/Sniffer', # use the URL to the github repo
-  download_url = "", # I'll explain this in a second
+  download_url = "https://github.com/dineshvipe/Sniffer/archive/0.2.tar.gz", # to download as tar.gz
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
